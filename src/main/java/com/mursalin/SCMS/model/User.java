bcb19f668527f9f49d82e375d29255727dcd9e2f
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-05 20:13:54 +0600

    validation added

--- src/main/java/com/mursalin/SCMS/model/User.java
+++ src/main/java/com/mursalin/SCMS/model/User.java
@@ -1,8 +1,12 @@
 package com.mursalin.SCMS.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,12 +21,24 @@ public class User {
 
     @Id
     private long userId;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String userName;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String userEmail;
+
     private boolean isEnable;
+
     private Role role = Role.USER;
 
-    @OneToMany
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Complaint> complaints;
+
 }
