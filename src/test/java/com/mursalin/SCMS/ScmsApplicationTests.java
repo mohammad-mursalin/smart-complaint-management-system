1a82e6a47ddd46e00dc5fe6634f331c937f128ac
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-05 09:26:22 +0600

    initial comment

---
+++ src/test/java/com/mursalin/SCMS/ScmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mursalin.SCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
