8b9354580714f483c3ef3b499bf3148842c84969
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-05 20:24:22 +0600

    repositories

---
+++ src/main/java/com/mursalin/SCMS/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mursalin.SCMS.repository;
+
+import com.mursalin.SCMS.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
