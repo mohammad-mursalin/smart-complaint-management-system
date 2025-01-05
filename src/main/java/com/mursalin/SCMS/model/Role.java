77c4703ec0f177e1f6c7432ece82a885cb86d985
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-05 18:45:57 +0600

    user entity

---
+++ src/main/java/com/mursalin/SCMS/model/Role.java
@@ -0,0 +1,5 @@
+package com.mursalin.SCMS.model;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/mursalin/SCMS/model/User.java
@@ -0,0 +1,21 @@
+package com.mursalin.SCMS.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private long userId;
+    private String userName;
+    private String password;
+    private String userEmail;
+    private Role role = Role.USER;
+}
