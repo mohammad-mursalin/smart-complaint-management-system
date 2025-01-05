f86a2a7bb5fc179c4f043d7b6f93ef34ec891044
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-05 20:22:17 +0600

    validation added

--- src/main/java/com/mursalin/SCMS/model/Complaint.java
+++ src/main/java/com/mursalin/SCMS/model/Complaint.java
@@ -2,6 +2,8 @@ package com.mursalin.SCMS.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,12 +19,23 @@ public class Complaint {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long complaintId;
+
+    @NotBlank(message = "Title is required")
+    @Size(min = 5, max = 100, message = "Title must be between 5 and 100 characters")
     private String title;
+
+    @NotBlank(message = "Description is required")
+    @Size(min = 10, max = 500, message = "Description must be between 10 and 500 characters")
     private String description;
+
     private String category;
+
     private String status;
+
     private LocalDate createdAt;
+
     private LocalDate updatedAt;
+
     private String imageName;
     private String imageType;
     @Lob
