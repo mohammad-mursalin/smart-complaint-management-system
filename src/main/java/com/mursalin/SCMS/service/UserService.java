695bb829064715d84922f19d5795e77183404afa
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-05 21:00:28 +0600

    user service

---
+++ src/main/java/com/mursalin/SCMS/service/UserService.java
@@ -0,0 +1,8 @@
+package com.mursalin.SCMS.service;
+
+import com.mursalin.SCMS.model.User;
+import org.springframework.http.ResponseEntity;
+
+public interface UserService {
+    ResponseEntity<?> register(User user);
+}
