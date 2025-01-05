95c368e8134679cecbbe7e07c31201d0ff90dbc0
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-05 19:56:19 +0600

    complaint entity

---
+++ src/main/java/com/mursalin/SCMS/model/Complaint.java
@@ -0,0 +1,32 @@
+package com.mursalin.SCMS.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Complaint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long complaintId;
+    private String title;
+    private String description;
+    private String category;
+    private String status;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+
+    @ManyToOne
+    private User user;
+}
