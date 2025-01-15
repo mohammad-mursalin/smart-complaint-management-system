3dc70a6848ae9b9a8f91c8bc0f5f5a59c02bfe60
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-15 21:06:55 +0600

    service annotation

--- src/main/java/com/mursalin/SCMS/service/impl/AdminServiceImpl.java
+++ src/main/java/com/mursalin/SCMS/service/impl/AdminServiceImpl.java
@@ -2,7 +2,9 @@ package com.mursalin.SCMS.service.impl;
 
 import com.mursalin.SCMS.service.AdminService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdminServiceImpl implements AdminService {
     @Override
     public ResponseEntity<?> getComplaints() {
