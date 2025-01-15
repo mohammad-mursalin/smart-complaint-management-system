63fed0f54e84d301b80b4b95b4e1c115460f9c9b
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-15 09:09:24 +0600

    id data type changed from primitive to oject

--- src/main/java/com/mursalin/SCMS/model/User.java
+++ src/main/java/com/mursalin/SCMS/model/User.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class User {
 
     @Id
-    private long userId;
+    private Long userId;
 
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
