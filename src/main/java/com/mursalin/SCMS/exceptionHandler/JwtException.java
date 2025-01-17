5958190182c5f980048bc69041bfa19b47cd14e8
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-17 18:24:33 +0600

    new exception class

---
+++ src/main/java/com/mursalin/SCMS/exceptionHandler/JwtException.java
@@ -0,0 +1,12 @@
+package com.mursalin.SCMS.exceptionHandler;
+
+public class JwtException extends RuntimeException {
+    public JwtException(String message) {
+        super(message);
+    }
+
+    public JwtException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
