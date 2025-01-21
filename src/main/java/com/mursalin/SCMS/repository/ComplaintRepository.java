9e4660740aa73d2d3cfa82cff659655363606006
mohammad-mursalin <mursalinlamon@gmail.com>
2025-01-22 00:03:27 +0600

    updated complaint repository

--- src/main/java/com/mursalin/SCMS/repository/ComplaintRepository.java
+++ src/main/java/com/mursalin/SCMS/repository/ComplaintRepository.java
@@ -7,14 +7,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
 
     @Query("SELECT new com.mursalin.SCMS.dto.ComplaintDTO(c.complaintId, c.title, c.description, c.category, c.status, " +
-            "c.createdAt, c.updatedAt, c.imageUrl) " +
-            "FROM Complaint c WHERE c.user.userId = :userId")
+            "c.createdAt, c.updatedAt, c.imageUrl, " +
+            "new com.mursalin.SCMS.dto.CommentDTO(cm.commentId, cm.comment, cm.createdAt, cm.editedAt, cm.commentedBy)) " +
+            "FROM Complaint c LEFT JOIN FETCH c.comments cm WHERE c.user.userId = :userId")
     List<ComplaintDTO> findComplaintsByUserId(Long userId);
+
+    @Query("SELECT c FROM Complaint c WHERE c.complaintId = :complaintId AND c.user.userEmail = :userEmail")
+    Optional<Complaint> findComplaintByIdAndUserEmail(Long complaintId, String userEmail);
 
 
     @Query("SELECT new com.mursalin.SCMS.dto.ComplaintDTO(c.complaintId, c.title, c.description, c.category, c.status, " +
